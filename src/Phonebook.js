e693a22700e34687e2273ab92ed86c219123bffc
anx <sebastian.glaser@digitalcareerinstitute.org>
2020-03-02 16:39:20 +0100

    refactor: add comments

--- src/Phonebook.js
+++ src/Phonebook.js
@@ -1,12 +1,16 @@
 
 import React        from 'react';
-import { Provider } from 'react-redux'
-import Add          from './Add'
-import List         from './List'
-import store        from './redux'
 
-export default function() {
-  return (
+import { Provider } from 'react-redux'  // Make the store available to
+                                        // child components
+
+import store        from './redux'      // The state-logic of our
+                                        // application (reducer, actions)
+
+import Add          from './Add'        // Form to add new entries
+import List         from './List'       // List of all entries
+
+export default function() { return (
   <Provider store={store}>
     <Add/>
     <List/>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 
 import * as serviceWorker from './serviceWorker';
+
 import React              from 'react';
 import ReactDOM           from 'react-dom';
+
 import Phonebook          from './Phonebook';
 
 ReactDOM.render( <Phonebook/>, document.getElementById('root') );
+
+/*
+  In order to allow the user to install our app to his Phone
+  we need to register a serviceWorker:
+    - Downloads all assets in the background and checks for updates (payload)
+    - Is required for advanced features like notifications, payment, etc.
+*/
 
 serviceWorker.register();
